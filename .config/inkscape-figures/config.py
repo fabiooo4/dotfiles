fc60e27478f0bbe6749f521f72b509f7812932ed
fabiooo4 <fabioirimie@gmail.com>
2024-10-09 10:56:00 +0200

    updated inkscape-figures config

--- .config/inkscape-figures/config.py
+++ .config/inkscape-figures/config.py
@@ -1,5 +1,5 @@
 def latex_template(name, title):
-    return '\n'.join((r"\begin{figure}[ht]",
+    return '\n'.join((r"\begin{figure}[H]",
                       r"    \centering",
                       rf"    \incfig[1]{{{name}}}",
                       rf"    \caption{{{title}}}",
